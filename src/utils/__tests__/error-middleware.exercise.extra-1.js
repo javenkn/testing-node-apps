a189c072049146b22cf6255b29f6db9cd67d72a2
javenkn <javenkn7@gmail.com>
2019-12-18 15:33:33 -1000

    Uses factories to clean up and remove duplication of code

---
+++ src/utils/__tests__/error-middleware.exercise.extra-1.js
@@ -0,0 +1,67 @@
+// Testing Middleware
+
+import {UnauthorizedError} from 'express-jwt'
+import errorMiddleware from '../error-middleware'
+
+function buildRes(overrides) {
+  const res = {
+    json: jest.fn(() => res),
+    status: jest.fn(() => res),
+    ...overrides,
+  }
+  return res
+}
+
+test('responds with 401 for express-jwt UnauthorizedError', () => {
+  const req = {}
+  const res = buildRes()
+  const next = jest.fn()
+  const code = 'some_error_code'
+  const message = 'Some message'
+  const error = new UnauthorizedError(code, {
+    message,
+  })
+  errorMiddleware(error, req, res, next)
+
+  expect(next).not.toHaveBeenCalled()
+
+  expect(res.status).toHaveBeenCalledWith(401)
+  expect(res.status).toHaveBeenCalledTimes(1)
+
+  expect(res.json).toHaveBeenCalledWith({
+    code: error.code,
+    message: error.message,
+  })
+  expect(res.json).toHaveBeenCalledTimes(1)
+})
+
+test('calls next if headersSent is true', () => {
+  const req = {}
+  const res = buildRes({headersSent: true})
+  const next = jest.fn()
+  const error = new Error('blah')
+  errorMiddleware(error, req, res, next)
+  expect(next).toHaveBeenCalledWith(error)
+  expect(next).toHaveBeenCalledTimes(1)
+  expect(res.status).not.toHaveBeenCalled()
+  expect(res.json).not.toHaveBeenCalled()
+})
+
+test('responds with 500 and error object', () => {
+  const req = {}
+  const res = buildRes()
+  const next = jest.fn()
+  const error = new Error('blah')
+  errorMiddleware(error, req, res, next)
+
+  expect(next).not.toHaveBeenCalled()
+
+  expect(res.status).toHaveBeenCalledWith(500)
+  expect(res.status).toHaveBeenCalledTimes(1)
+
+  expect(res.json).toHaveBeenCalledWith({
+    message: error.message,
+    stack: error.stack,
+  })
+  expect(res.json).toHaveBeenCalledTimes(1)
+})
---
+++ src/utils/__tests__/error-middleware.exercise.extra-2.js
@@ -0,0 +1,59 @@
+// Testing Middleware
+
+import {UnauthorizedError} from 'express-jwt'
+import {buildRes, buildReq, buildNext} from 'utils/generate'
+import errorMiddleware from '../error-middleware'
+
+test('responds with 401 for express-jwt UnauthorizedError', () => {
+  const req = buildReq()
+  const res = buildRes()
+  const next = buildNext()
+  const code = 'some_error_code'
+  const message = 'Some message'
+  const error = new UnauthorizedError(code, {
+    message,
+  })
+  errorMiddleware(error, req, res, next)
+
+  expect(next).not.toHaveBeenCalled()
+
+  expect(res.status).toHaveBeenCalledWith(401)
+  expect(res.status).toHaveBeenCalledTimes(1)
+
+  expect(res.json).toHaveBeenCalledWith({
+    code: error.code,
+    message: error.message,
+  })
+  expect(res.json).toHaveBeenCalledTimes(1)
+})
+
+test('calls next if headersSent is true', () => {
+  const req = buildReq()
+  const res = buildRes({headersSent: true})
+  const next = buildNext()
+  const error = new Error('blah')
+  errorMiddleware(error, req, res, next)
+  expect(next).toHaveBeenCalledWith(error)
+  expect(next).toHaveBeenCalledTimes(1)
+  expect(res.status).not.toHaveBeenCalled()
+  expect(res.json).not.toHaveBeenCalled()
+})
+
+test('responds with 500 and error object', () => {
+  const req = buildReq()
+  const res = buildRes()
+  const next = buildNext()
+  const error = new Error('blah')
+  errorMiddleware(error, req, res, next)
+
+  expect(next).not.toHaveBeenCalled()
+
+  expect(res.status).toHaveBeenCalledWith(500)
+  expect(res.status).toHaveBeenCalledTimes(1)
+
+  expect(res.json).toHaveBeenCalledWith({
+    message: error.message,
+    stack: error.stack,
+  })
+  expect(res.json).toHaveBeenCalledTimes(1)
+})
