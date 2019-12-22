daa9946841708a785b521e0dc6c7437bc80bca3d
javenkn <javenkn7@gmail.com>
2019-12-21 19:23:30 -1000

    Adds test for deleteListItem

--- src/routes/__tests__/list-items-controller.exercise.extra-2.js
+++ src/routes/__tests__/list-items-controller.exercise.extra-2.js
@@ -259,3 +259,18 @@ test('updateListItem updates an existing list item', async () => {
   })
   expect(res.json).toHaveBeenCalledTimes(1)
 })
+
+test('deleteListItem removes an existing list item', async () => {
+  const listItem = buildListItem()
+
+  const req = buildReq({listItem})
+  const res = buildRes()
+
+  await listItemsController.deleteListItem(req, res)
+
+  expect(listItemsDB.remove).toHaveBeenCalledWith(listItem.id)
+  expect(listItemsDB.remove).toHaveBeenCalledTimes(1)
+
+  expect(res.json).toHaveBeenCalledWith({success: true})
+  expect(res.json).toHaveBeenCalledTimes(1)
+})
